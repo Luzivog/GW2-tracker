b2f9c55929d59e5fb30533c7af89f7ba5e59e00c
Luzivog <thpfai@gmail.com>
2022-12-22 09:09:41 -0500

    mc bug fixed

--- Account.js
+++ Account.js
@@ -2,9 +2,9 @@ const { fetch } = require('cross-fetch');
 
 const { API_URL } = require('./config');
 
-const call_api = async (api_key, endpoint, params={}) => {
+const call_api = async (endpoint, api_key=null, params={}) => {
 
-    params['access_token'] = api_key;
+    if (api_key) params['access_token'] = api_key;
     params_list = [];
     for (k in params) params_list.push(`${k}=${params[k]}`);
     params_str = params_list.join('&');
@@ -24,15 +24,15 @@ class Account {
 
     async init() {
 
-        let account = await call_api(this.api_key, 'account');
+        let account = await call_api('account', this.api_key);
         if (!Object.keys(account).includes('id'))  throw new TypeError('Invalid api key');
 
         this.account = account;
-        this.characters = await call_api(this.api_key, 'characters', {'ids':'all'});
+        this.characters = await call_api('characters', this.api_key, {'ids':'all'});
 
-        this.main_character = this.characters.sort(c =>  -c['age'])[0];
+        this.main_character = this.characters.sort((c1,c2) =>  c2['age']-c1['age'])[0];
 
-        let wallet = await call_api(this.api_key, 'account/wallet');
+        let wallet = await call_api('account/wallet', this.api_key);
         this.wallet = wallet;
 
         let coins = wallet[0]['value'];
@@ -41,9 +41,12 @@ class Account {
         this.silver = coins > 99 ? `${+`${coins}`.slice(-4,-2)}` : '0';
         this.bronze = `${+`${coins}`.slice(-2)}`;
 
+        this.achievements = await call_api('account/achievements', this.api_key);
+
     };
 };
 
 module.exports = {
-    Account: Account
+    Account: Account,
+    call_api: call_api
 };
